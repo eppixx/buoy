735c6d9879d67bfbc7dd41d44c4158b30ec27f23
Thomas Eppers <thomas@Eppi-PC.fritz.box>
2023-09-17 20:14:35 +0200

    fixed redundant creation of clients

--- src/client.rs
+++ src/client.rs
@@ -11,27 +11,30 @@ pub struct Client {
 impl Client {
     pub fn get() -> &'static Mutex<Client> {
         static CLIENT: OnceLock<Mutex<Client>> = OnceLock::new();
-        {
-            let settings = Settings::get().lock().unwrap();
-            if let (Some(uri), Some(user), Some(hash), Some(salt)) = (
-                &settings.login_uri,
-                &settings.login_username,
-                &settings.login_hash,
-                &settings.login_salt,
-            ) {
-                let auth = submarine::auth::Auth {
-                    user: user.clone(),
-                    version: String::from("0.16.1"),
-                    client_name: String::from("Bouy"),
-                    hash: hash.clone(),
-                    salt: salt.clone(),
-                };
-                let client = Client {
-                    inner: Some(submarine::Client::new(uri, auth)),
-                };
-                CLIENT.get_or_init(|| Mutex::new(client))
-            } else {
-                CLIENT.get_or_init(|| Mutex::new(Client::default()))
+        match CLIENT.get() {
+            Some(client) => return client,
+            None => {
+                let settings = Settings::get().lock().unwrap();
+                if let (Some(uri), Some(user), Some(hash), Some(salt)) = (
+                    &settings.login_uri,
+                    &settings.login_username,
+                    &settings.login_hash,
+                    &settings.login_salt,
+                ) {
+                    let auth = submarine::auth::Auth {
+                        user: user.clone(),
+                        version: String::from("0.16.1"),
+                        client_name: String::from("Bouy"),
+                        hash: hash.clone(),
+                        salt: salt.clone(),
+                    };
+                    let client = Client {
+                        inner: Some(submarine::Client::new(uri, auth)),
+                    };
+                    CLIENT.get_or_init(|| Mutex::new(client))
+                } else {
+                    CLIENT.get_or_init(|| Mutex::new(Client::default()))
+                }
             }
         }
     }
