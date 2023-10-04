f08ea8450e76479b47063bff332bb9d5280a1b16
Thomas Eppers <epperst@hochschule-trier.de>
2023-10-04 21:39:34 +0200

    fix styling issues of cover in AlbumView

--- src/components/album_view.rs
+++ src/components/album_view.rs
@@ -105,7 +105,7 @@ impl relm4::Component for AlbumView {
 
                 gtk::Box {
                     set_orientation: gtk::Orientation::Vertical,
-                    set_spacing: 15,
+                    set_spacing: 8,
 
                     gtk::Label {
                         add_css_class: "h3",
