8f5b33a879192a220d67f868b6d728ea2423149c
Thomas Eppers <epperst@hochschule-trier.de>
2024-06-27 08:54:33 +0200

    added blank implementations for mpris

--- src/mpris.rs
+++ src/mpris.rs
@@ -1,84 +1,23 @@
-use zbus::{interface, Connection, Result};
+use std::collections::HashMap;
 
-use std::sync::{Arc, Mutex, RwLock};
-
-struct Greeter {
-    name: String,
-    sender: async_channel::Sender<MprisOut>,
-}
-
-#[interface(name = "org.zbus.MyGreeter1")]
-impl Greeter {
-    async fn say_hello(&self, name: &str) -> String {
-        format!("Hello {}!", name)
-    }
-
-    // Rude!
-    async fn go_away(
-        &self,
-        #[zbus(signal_context)] ctxt: zbus::SignalContext<'_>,
-    ) -> zbus::fdo::Result<()> {
-        Self::greeted_everyone(&ctxt).await?;
-        self.sender
-            .try_send(MprisOut::DisplayToast(String::from("hello from mpris")))
-            .unwrap();
-
-        Ok(())
-    }
-
-    /// A "GreeterName" property.
-    #[zbus(property)]
-    async fn greeter_name(&self) -> &str {
-        &self.name
-    }
-
-    /// A setter for the "GreeterName" property.
-    ///
-    /// Additionally, a `greeter_name_changed` method has been generated for you if you need to
-    /// notify listeners that "GreeterName" was updated. It will be automatically called when
-    /// using this setter.
-    #[zbus(property)]
-    async fn set_greeter_name(&mut self, name: String) {
-        self.name = name;
-    }
-
-    /// A signal; the implementation is provided by the macro.
-    #[zbus(signal)]
-    async fn greeted_everyone(ctxt: &zbus::SignalContext<'_>) -> Result<()>;
-}
+use zbus::interface;
 
 #[derive(Debug)]
 pub struct Mpris {
-    // root: zbus::Connection,
-    greeter: zbus::Connection,
+    _connection: zbus::Connection,
 }
 
 impl Mpris {
     pub async fn new(sender: &async_channel::Sender<MprisOut>) -> anyhow::Result<Mpris> {
-        let root = Root::new(sender).await?;
-        // let root_connection = zbus::conn::Builder::session()?
-        //     .name("org.mpris.MediaPlayer2.buoy")?
-        //     .serve_at("/org/mpris/MediaPlayer2/buoy", root)?
-        //     .build()
-        //     .await?;
-
-        // let mpris = Mpris {
-        //     root: root_connection,
-        // };
-        let greeter = Greeter {
-            name: "GreeterName".to_string(),
-            sender: sender.clone(),
-        };
+        let root = Root::new(&sender.clone());
+        let player = Player::new(sender);
         let connection = zbus::conn::Builder::session()?
             .name("org.mpris.MediaPlayer2.buoy")?
-            .serve_at("/org/mpris/MediaPlayer2", greeter)?
+            .serve_at("/org/mpris/MediaPlayer2", root)?
+            .serve_at("/org/mpris/MediaPlayer2", player)?
             .build()
             .await?;
-
-        // Ok(Mpris { root: connection, })
-        Ok(Mpris {
-            greeter: connection,
-        })
+        Ok(Mpris { _connection: connection, })
     }
 }
 
@@ -100,15 +39,12 @@ pub enum MprisOut {
 }
 
 impl Root {
-    async fn new(sender: &async_channel::Sender<MprisOut>) -> anyhow::Result<Self> {
-        let result = Self {
-            sender: sender.clone(),
-        };
-
-        Ok(result)
+    fn new(sender: &async_channel::Sender<MprisOut>) -> Self {
+        Self { sender: sender.clone() }
     }
 }
 
+// implements https://specifications.freedesktop.org/mpris-spec/latest/Media_Player.html
 #[interface(name = "org.mpris.MediaPlayer2")]
 impl Root {
     fn raise(&self) {
@@ -119,42 +55,37 @@ impl Root {
         self.sender.try_send(MprisOut::WindowQuit).unwrap();
     }
 
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn can_quit(&self) -> bool {
         true
     }
 
-    #[dbus_interface(property)]
-    fn fullscreen(&self) -> bool {
-        false
-    }
-
-    #[dbus_interface(property)]
-    fn can_set_fullscreen(&self) -> bool {
-        false
-    }
-
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn can_raise(&self) -> bool {
         true
     }
 
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn has_track_list(&self) -> bool {
         true
     }
 
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn identity(&self) -> &str {
+        "Buoy"
+    }
+
+    #[zbus(property)]
+    fn desktop_entry(&self) -> &str {
         "buoy"
     }
 
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn supported_uri_schemes(&self) -> Vec<&str> {
         vec![]
     }
 
-    #[dbus_interface(property)]
+    #[zbus(property)]
     fn supported_mime_types(&self) -> Vec<&str> {
         vec![]
     }
@@ -164,6 +95,13 @@ struct Player {
     sender: async_channel::Sender<MprisOut>,
 }
 
+impl Player {
+    fn new(sender: &async_channel::Sender<MprisOut>) -> Self {
+        Self { sender: sender.clone() }
+    }
+}
+
+// implementes https://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html
 #[interface(name = "org.mpris.MediaPlayer2.Player")]
 impl Player {
     fn next(&self) {
@@ -182,186 +120,188 @@ impl Player {
         self.sender.try_send(MprisOut::PlayPause).unwrap();
     }
 
-    fn play(&self) {
-        self.sender.try_send(MprisOut::Play).unwrap();
-    }
-
     fn stop(&self) {
         self.sender.try_send(MprisOut::Stop).unwrap();
     }
 
-    // fn seek(&self, offset: i32) {
-    //     self.sender
-    //         .try_send(app_action::AppAction::Queue(
-    //             app_action::QueueRequest::SeekOffset(offset),
-    //         ))
-    //         .unwrap();
-    // }
+    fn play(&self) {
+        self.sender.try_send(MprisOut::Play).unwrap();
+    }
 
-    // fn set_position(&self, _index: i32, pos: i32) {
-    //     //TODO implement changing index
-    //     self.sender
-    //         .try_send(app_action::AppAction::Queue(
-    //             app_action::QueueRequest::Seek(pos),
-    //         ))
-    //         .unwrap();
-    // }
+    /// * `offset` - Position relative to current position to seek to in microseconds
+    fn seek(&self, offset: i32) {
+        // self.sender.try_send(MprisOut::Seek(offset)).unwrap();
+        //TODO
+    }
 
-    // fn open_uri(&self, _uri: &str) {}
+    /// * `index` - Index id of the track to set to
+    /// * `pos` - Position to seek to in micoseconds
+    fn set_position(&self, index: i32, pos: i32) {
+        // self.sender.try_send(MprisOut::SetSeekPosition(index, pos)).unwrap()
+        //TODO
+    }
 
-    // //Playing, Paused, Stopped
-    // #[dbus_interface(property)]
-    // pub fn playback_status(&self) -> zvariant::Value {
-    //     zvariant::Value::new(String::from(&self.settings.read().unwrap().queue_state))
-    // }
+    fn open_uri(&self, _uri: &str) {}
 
-    // //None, Track, Playlist
-    // #[dbus_interface(property)]
-    // pub fn loop_status(&self) -> zvariant::Value {
-    //     zvariant::Value::new(String::from(self.settings.read().unwrap().queue_repeat()))
-    // }
+    //Playing, Paused, Stopped
+    #[zbus(property)]
+    pub fn playback_status(&self) -> zvariant::Value {
+        // zvariant::Value::new(String::from(&self.settings.read().unwrap().queue_state))
+        //TODO
+        zvariant::Value::new("Playing")
+    }
 
-    // #[dbus_interface(property)]
-    // fn set_loop_status(&mut self, loop_status: &str) {
-    //     let mut settings = self.settings.write().unwrap();
-    //     settings.set_queue_repeat(repeat::Repeat::from(loop_status));
-    // }
+    //None, Track, Playlist
+    #[zbus(property)]
+    pub fn loop_status(&self) -> zvariant::Value {
+        // zvariant::Value::new(String::from(self.settings.read().unwrap().queue_repeat()))
+        //TODO
+        zvariant::Value::new(false)
+    }
 
-    // //playback speed; 1.0 is normal speed, 0.5 is half speed
-    // #[dbus_interface(property)]
-    // fn rate(&self) -> f64 {
-    //     1.0
-    // }
+    #[zbus(property)]
+    fn set_loop_status(&mut self, loop_status: &str) {
+        // zvariant::Value::new(String::from(self.settings.read().unwrap().queue_repeat()))
+        //TODO
+    }
 
-    // //dont support changing playback speed
-    // #[dbus_interface(property)]
-    // fn set_rate(&mut self, _rate: f64) {}
+    //playback speed; 1.0 is normal speed, 0.5 is half speed
+    #[zbus(property)]
+    fn rate(&self) -> f64 {
+        1.0
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn shuffle(&self) -> zvariant::Value {
-    //     let settings = self.settings.read().unwrap();
-    //     match settings.queue_random() {
-    //         shuffle::Shuffle::Sequential => zvariant::Value::new(false),
-    //         shuffle::Shuffle::Random => zvariant::Value::new(true),
-    //     }
-    // }
+    #[zbus(property)]
+    fn set_rate(&self, _rate: f64) {}
 
-    // #[dbus_interface(property)]
-    // fn set_shuffle(&mut self, shuffle: bool) {
-    //     let mut settings = self.settings.write().unwrap();
-    //     let random = match shuffle {
-    //         true => shuffle::Shuffle::Random,
-    //         false => shuffle::Shuffle::Sequential,
-    //     };
-    //     settings.set_queue_random(random);
-    // }
+    #[zbus(property)]
+    pub fn shuffle(&self) -> zvariant::Value {
+        //TODO
+        zvariant::Value::new(false)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn metadata(&self) -> zvariant::Value {
-    //     let mut map = HashMap::new();
-    //     let track = self.playing_track.lock().unwrap();
-    //     if let Some(track) = &*track {
-    //         use zvariant::Value;
-    //         map.insert("mpris:trackid", Value::new(String::from(track.id())));
-    //         // from sec to ms
-    //         map.insert("mpris:length", Value::new(track.length() * 1000));
-    //         map.insert("xesam:title", Value::new(String::from(track.title())));
-    //         if let Some(album) = &track.album() {
-    //             map.insert("xesam:album", Value::new(String::from(album)));
-    //         }
-    //         if let Some(artist) = &track.artist() {
-    //             map.insert("xesam:albumArtist", Value::new(String::from(artist)));
-    //         }
-    //         if let Some(artist) = &track.artist() {
-    //             map.insert("xesam:artist", Value::new(vec![String::from(artist)]));
-    //         }
-    //         if let Some(number) = &track.disc_number() {
-    //             map.insert("xesam:discNumber", zvariant::Value::new(*number));
-    //         }
-    //         if let Some(number) = &track.track_number() {
-    //             map.insert("xesam:trackNumber", zvariant::Value::new(*number));
-    //         }
-    //         if let Some(url) = &self.cover_url {
-    //             map.insert("mpris:artUrl", Value::new(url));
-    //         }
-    //         //TODO
-    //         // map.insert("xesam:useCount", zvariant::Value::new(5));
-    //         // map.insert("xesam:genre", zvariant::Value::new(vec!["Blues", "Phonk"]));
-    //     }
+    #[zbus(property)]
+    fn set_shuffle(&mut self, shuffle: bool) {
+        // let mut settings = self.settings.write().unwrap();
+        // let random = match shuffle {
+        //     true => shuffle::Shuffle::Random,
+        //     false => shuffle::Shuffle::Sequential,
+        // };
+        // settings.set_queue_random(random);
+        //TODO
+    }
 
-    //     zvariant::Value::new(map)
-    // }
+    #[zbus(property)]
+    pub fn metadata(&self) -> zvariant::Value {
+        let mut map = HashMap::new();
+        // let track = self.playing_track.lock().unwrap();
+        // if let Some(track) = &*track {
+        //     use zvariant::Value;
+        //     map.insert("mpris:trackid", Value::new(String::from(track.id())));
+        //     // from sec to ms
+        //     map.insert("mpris:length", Value::new(track.length() * 1000));
+        //     map.insert("xesam:title", Value::new(String::from(track.title())));
+        //     if let Some(album) = &track.album() {
+        //         map.insert("xesam:album", Value::new(String::from(album)));
+        //     }
+        //     if let Some(artist) = &track.artist() {
+        //         map.insert("xesam:albumArtist", Value::new(String::from(artist)));
+        //     }
+        //     if let Some(artist) = &track.artist() {
+        //         map.insert("xesam:artist", Value::new(vec![String::from(artist)]));
+        //     }
+        //     if let Some(number) = &track.disc_number() {
+        //         map.insert("xesam:discNumber", zvariant::Value::new(*number));
+        //     }
+        //     if let Some(number) = &track.track_number() {
+        //         map.insert("xesam:trackNumber", zvariant::Value::new(*number));
+        //     }
+        //     if let Some(url) = &self.cover_url {
+        //         map.insert("mpris:artUrl", Value::new(url));
+        //     }
+        //     //TODO
+        //     // map.insert("xesam:useCount", zvariant::Value::new(5));
+        //     // map.insert("xesam:genre", zvariant::Value::new(vec!["Blues", "Phonk"]));
+        // }
 
-    // //ranges from 0.0 to 1.0
-    // #[dbus_interface(property)]
-    // pub fn volume(&self) -> zvariant::Value {
-    //     zvariant::Value::new(self.volume)
-    // }
+        //TODO
+        map.insert("xesam:title", zvariant::Value::new(String::from("Test title")));
+        zvariant::Value::new(map)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn set_volume(&mut self, volume: f64) {
-    //     self.settings.write().unwrap().set_volume(volume);
-    // }
+    //ranges from 0.0 to 1.0
+    #[dbus_interface(property)]
+    pub fn volume(&self) -> zvariant::Value {
+        // zvariant::Value::new(self.volume)
+        zvariant::Value::new(1.0f64)
+    }
 
-    // //time im mircoseconds
-    // #[dbus_interface(property)]
-    // fn position(&self) -> zvariant::Value {
-    //     // TODO
-    //     zvariant::Value::new(5000)
-    // }
+    #[dbus_interface(property)]
+    pub fn set_volume(&mut self, volume: f64) {
+        // self.settings.write().unwrap().set_volume(volume);
+        //TODO
+    }
 
-    // #[dbus_interface(property)]
-    // fn minimum_rate(&self) -> zvariant::Value {
-    //     zvariant::Value::new(1.0f64)
-    // }
+    //time im mircoseconds
+    #[dbus_interface(property)]
+    fn position(&self) -> zvariant::Value {
+        // TODO
+        zvariant::Value::new(5000)
+    }
 
-    // #[dbus_interface(property)]
-    // fn maximum_rate(&self) -> zvariant::Value {
-    //     zvariant::Value::new(1.0f64)
-    // }
+    #[dbus_interface(property)]
+    fn minimum_rate(&self) -> zvariant::Value {
+        zvariant::Value::new(1.0f64)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn can_go_next(&self) -> zvariant::Value {
-    //     zvariant::Value::new(!self.queue_empty)
-    // }
+    #[dbus_interface(property)]
+    fn maximum_rate(&self) -> zvariant::Value {
+        zvariant::Value::new(1.0f64)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn can_go_previous(&self) -> zvariant::Value {
-    //     zvariant::Value::new(!self.queue_empty)
-    // }
+    #[dbus_interface(property)]
+    pub fn can_go_next(&self) -> zvariant::Value {
+        // TODO
+        zvariant::Value::new(true)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn can_play(&self) -> zvariant::Value {
-    //     let play = match self.settings.read().unwrap().queue_state {
-    //         queue_state::State::Pause(_) => true,
-    //         queue_state::State::Stop if self.queue_empty => true,
-    //         _ => false,
-    //     };
-    //     zvariant::Value::new(play)
-    // }
+    #[dbus_interface(property)]
+    pub fn can_go_previous(&self) -> zvariant::Value {
+        // TODO
+        zvariant::Value::new(true)
+    }
 
-    // #[dbus_interface(property)]
-    // pub fn can_pause(&self) -> zvariant::Value {
-    //     if let queue_state::State::Play(_) = self.settings.read().unwrap().queue_state {
-    //         return zvariant::Value::new(true);
-    //     }
+    #[dbus_interface(property)]
+    pub fn can_play(&self) -> zvariant::Value {
+        // TODO
+        // let play = match self.settings.read().unwrap().queue_state {
+        //     queue_state::State::Pause(_) => true,
+        //     queue_state::State::Stop if self.queue_empty => true,
+        //     _ => false,
+        // };
+        // zvariant::Value::new(play)
+        zvariant::Value::new(true)
+    }
 
-    //     zvariant::Value::new(false)
-    // }
+    #[dbus_interface(property)]
+    pub fn can_pause(&self) -> zvariant::Value {
+        // TODO
+        // if let queue_state::State::Play(_) = self.settings.read().unwrap().queue_state {
+        //     return zvariant::Value::new(true);
+        // }
+
+        zvariant::Value::new(false)
+    }
 
     #[dbus_interface(property)]
     pub fn can_seek(&self) -> zvariant::Value {
+        // TODO
         zvariant::Value::new(false)
     }
 
     #[dbus_interface(property)]
     fn can_control(&self) -> zvariant::Value {
+        // TODO
         zvariant::Value::new(true)
     }
-
-    // #[dbus_interface(signal)]
-    // pub async fn seeked(&self, ctxt: &zbus::SignalContext<'_>) -> zbus::Result<()> {
-    //     Ok(())
-    //     // TODO
-    // }
 }
