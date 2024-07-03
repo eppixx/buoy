982a26b2d87a2ef4d0d22118e6a7486cb434175b
Thomas Eppers <epperst@hochschule-trier.de>
2024-07-03 17:32:50 +0200

    formatting

--- src/gtk_helper/stack.rs
+++ src/gtk_helper/stack.rs
@@ -3,13 +3,21 @@ use relm4::gtk::{self, glib};
 use std::fmt::Display;
 
 pub trait StackExt {
-    fn add_enumed<T: Display + TryFrom<String>>(&self, child:  &impl glib::object::IsA<gtk::Widget>, state: T) -> gtk::StackPage;
+    fn add_enumed<T: Display + TryFrom<String>>(
+        &self,
+        child: &impl glib::object::IsA<gtk::Widget>,
+        state: T,
+    ) -> gtk::StackPage;
     fn set_visible_child_enum<T: Display + TryFrom<String>>(&self, state: &T);
     fn visible_child_enum<T: TryFrom<String>>(&self) -> Option<T>;
 }
 
 impl StackExt for gtk::Stack {
-    fn add_enumed<T: Display>(&self, child: &impl glib::object::IsA<gtk::Widget>, state: T) -> gtk::StackPage {
+    fn add_enumed<T: Display>(
+        &self,
+        child: &impl glib::object::IsA<gtk::Widget>,
+        state: T,
+    ) -> gtk::StackPage {
         self.add_named(child, Some(&state.to_string()))
     }
 
@@ -18,12 +26,13 @@ impl StackExt for gtk::Stack {
     }
 
     fn visible_child_enum<T>(&self) -> Option<T>
-    where T: TryFrom<String>
+    where
+        T: TryFrom<String>,
     {
         let name = self.visible_child_name()?;
         match T::try_from(name.to_string()) {
             Err(_e) => None,
-            Ok(state) => Some(state)
+            Ok(state) => Some(state),
         }
     }
 }
