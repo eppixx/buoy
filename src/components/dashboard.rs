b8c87f6eee9da2d83e2a501c1567e21b2824c912
Thomas Eppers <epperst@hochschule-trier.de>
2025-04-10 11:06:25 +0200

    fixed not fetching last played albums in Dashboard

--- src/components/dashboard.rs
+++ src/components/dashboard.rs
@@ -128,7 +128,7 @@ impl relm4::Component for Dashboard {
             };
             DashboardCmd::LoadedRecentlyPlayed(
                 client
-                    .get_album_list2(
+                    .get_album_list(
                         submarine::api::get_album_list::Order::Recent,
                         Some(10),
                         None,
